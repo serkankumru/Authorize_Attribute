2c9c07b2739fe31b2d77911890d6ad8d285be828
Serkan Kumru <serkankumru1997@gmail.com>
2023-09-16 11:48:19 +0300

    Add files via upload

---
+++ AuthorizeEditor.cs
@@ -0,0 +1,28 @@
+﻿using DAL;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Haberler.Auth
+{
+    public class AuthorizeEditor : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Session["Editor"] != null)
+            {
+                Editor edt = (Editor)httpContext.Session["Editor"];
+                if (edt == null)
+                {
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Home/Login");
+        }
+    }
+}
